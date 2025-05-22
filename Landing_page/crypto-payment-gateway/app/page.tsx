b802e9401a54d66caf87ca235daef204048fd0a8
Dhruv <131877805+getnewone@users.noreply.github.com>
2025-05-22 14:19:43 +1000

    added github

--- Landing_page/crypto-payment-gateway/app/page.tsx
+++ Landing_page/crypto-payment-gateway/app/page.tsx
@@ -488,7 +488,7 @@ export default function Home() {
                     <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                   </svg>
                 </Link>
-                <Link href="#" className="text-muted-foreground hover:text-teal-400">
+                <Link href="https://github.com/My-CMDhub?tab=overview&from=2025-05-01&to=2025-05-22" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-teal-400">
                   <span className="sr-only">GitHub</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
